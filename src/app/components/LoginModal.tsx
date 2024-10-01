2a36d67dddca6439a10552981cfa30e74aadf38a
Ananth Prabhu T <prabhuanantht@gmail.com>
2024-10-01 19:47:05 +0530

    removed import useEffect

--- src/app/components/LoginModal.tsx
+++ src/app/components/LoginModal.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 // Define the types for user categories
 type UserType = 'Student' | 'Instructor' | 'School';
